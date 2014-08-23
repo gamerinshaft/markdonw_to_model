4f13fc9fb9223c0689c811b7050c4a6360c5f079
j.taneichi <j.tane3ma@gmail.com>
2014-08-23 13:50:00 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823044659) do
+ActiveRecord::Schema.define(version: 20140823044927) do
+
+  create_table "chapters", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "codes", force: true do |t|
     t.integer  "chapter_id"
